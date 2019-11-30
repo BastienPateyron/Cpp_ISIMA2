9dbb18bac6d282e9f03ead717a2437d6829a59ce
bapateyron <bastien.pateyron@etu.isima.fr>
2019-11-30 18:38:47 +0100

    All test passed

--- sms/sms.cpp
+++ sms/sms.cpp
@@ -1,1 +1,26 @@
 #include "sms.hpp"
+
+// Telephone
+std::string Telephone::getNumero() const {return num;}
+void        Telephone::setNumero(std::string num) {this->num = num;}
+Telephone::Telephone() {}
+Telephone::Telephone(std::string num) : num(num) {}
+bool Telephone::operator<(Telephone const & n) { return num < n.getNumero();}
+
+
+// Reseau
+void Reseau::ajouter(std::string const & num)
+{
+   telephones.push_back(Telephone(num));
+   telephones.sort();
+}
+
+std::string Reseau::lister() const
+{  
+   return std::accumulate(
+      telephones.begin(),
+      telephones.end(),
+      std::string(""),
+      [] (std::string s, Telephone const & t) { return s += t.getNumero() + '\n' ;}
+   );
+}
--- sms/sms.hpp
+++ sms/sms.hpp
@@ -1,5 +1,34 @@
 #ifndef __SMS
 #define __SMS
+#include <iostream>
+#include <list>
+#include <numeric>
+
+class Telephone
+{
+   private:
+      std::string num;
+   
+   public:
+      Telephone();
+      Telephone(std::string);
+      std::string getNumero() const;
+      void        setNumero(std::string);
+
+      bool operator<(Telephone const &);
+};
+
+
+
+class Reseau
+{
+   private:
+      std::list<Telephone> telephones;
+   
+   public:
+      void        ajouter(std::string const &);
+      std::string lister() const;         
+};
 
 
 
